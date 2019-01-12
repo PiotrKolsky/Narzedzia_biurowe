4c127aaa4ab50a0795801c6360a7e9ebd8ffc5ac
PiotrKolsky <31007985+PiotrKolsky@users.noreply.github.com>
2019-01-12 19:32:15 +0100

    Update Pdf.py

--- Pdf.py
+++ Pdf.py
@@ -3,4 +3,16 @@ df = read_pdf("Pelna_tabela_notowan_z_dnia_2017.05.25.pdf", encoding='latin2', e
 #lub np pages='1-3, 6'
 
 convert_into("Pelna_tabela_notowan_z_dnia_2017.05.25.pdf", "test.csv", output_format="csv", pages="all")
-!cat test.csv
+
+
+import camelot
+tables = camelot.read_pdf('brzozow.pdf', pages='all')
+tables
+
+tables.export('brzozow.csv', f='csv', compress=True) 
+tables[15]
+tables[15].parsing_report
+for i in range(len(tables)):
+    print(tables[i].df)
+tables[4].to_excel('brzozow.xlsx') 
+tables[7].df
